5759e03580f4e8b6b8835a938a2842458317098d
SouXx <frahm.tobias@gmail.com>
2018-11-02 14:35:00 +0100

    first commit

---
+++ main/lavor_main.c
@@ -0,0 +1,49 @@
+
+/*
+ * This code is part of the LaVOR system application. Please see the README.md
+ * file for further information.
+ *
+ * The code is licensed under the
+ *
+ * 		Creative Commmons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ *
+ * You are free to use this code for non-commercial applications as long as you leave this
+ * attribution included in all copies or substantial portions of the Software.
+ *
+ * Authors: Tobias Frahm, Philipp Haenyes, Joschka Sondhof, Josefine Palm, Malte Rejzek
+ */
+
+
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+
+void app_main()
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
