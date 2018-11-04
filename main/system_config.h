a0d224091a5f069918217e9abbd4c9d80b80b8e0
TobiasFr <frahm.tobias@gmail.com>
2018-11-04 16:36:49 +0100

    added WIFI config macros

--- main/system_config.h
+++ main/system_config.h
@@ -14,5 +14,13 @@
 #define MOTOR_DIR_OUT_PIN 				(18)
 #define MOTOR_PWM_OUT_PIN 				(32)
 #define PUBLISH_FREQUENCY				(750) /* Hz */
+#define CAPTURE_RISING					(1)
 
+//WIFI
+#define CONFIG_WIFI_SSID			"sRx"
+#define CONFIG_WIFI_PASSWORD		"7uzgl4TF"
+#define CONFIG_ESP_MAXIMUM_RETRY		(5)
+#define CONFIG_EXAMPLE_IPV4_ADDR	"89.15.239.250"
+#define CONFIG_EXAMPLE_IPV4			true
+#define CONFIG_EXAMPLE_PORT			(80)
 #endif /* MAIN_SYSTEM_CONFIG_H_ */
